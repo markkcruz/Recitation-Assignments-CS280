aea0c340662eccff8a67c72c810eb43e42f194d0
Mark K. Cruz <jink144@gmail.com>
2021-04-28 01:16:57 -0400

    Class/Struct Practice

---
+++ Log Class Practice by The Cherno/cmake-build-debug/CMakeFiles/Recitation_Assignment_8_Class_Practice.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Recitation_Assignment_8_Class_Practice.dir/log.cpp.obj"
+  "CMakeFiles/Recitation_Assignment_8_Class_Practice.dir/main.cpp.obj"
+  "Recitation_Assignment_8_Class_Practice.exe"
+  "Recitation_Assignment_8_Class_Practice.exe.manifest"
+  "Recitation_Assignment_8_Class_Practice.lib"
+  "Recitation_Assignment_8_Class_Practice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Recitation_Assignment_8_Class_Practice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
